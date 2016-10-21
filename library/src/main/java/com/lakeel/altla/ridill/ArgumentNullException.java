d7077356c4aaeb562cf7d370a1f1392d50bd0692
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2016-10-21 18:31:48 +0900

    [DOC] Write Javadoc

--- library/src/main/java/com/lakeel/altla/ridill/ArgumentNullException.java
+++ library/src/main/java/com/lakeel/altla/ridill/ArgumentNullException.java
@@ -1,7 +1,16 @@
 package com.lakeel.altla.ridill;
 
+/**
+ * The exception that is thrown when a null reference is passed to a method that
+ * does not accept it as a valid argument.
+ */
 public class ArgumentNullException extends IllegalArgumentException {
 
+    /**
+     * Initializes a new instance with the name of the parameter that causes this exception.
+     *
+     * @param paramName The name of the parameter.
+     */
     public ArgumentNullException(String paramName) {
         super(paramName);
     }
